1e62408245e55e48d414d5d29dbd026ac7306f74
Nightfall3594 <briankristoferperez28@gmail.com>
2026-01-09 15:43:23 +0800

    refactor: move the project from temp directory to root

--- my-app/src/components/pages/Console.tsx
+++
@@ -1,9 +0,0 @@
-import Terminal from "../components/console-page/Terminal.tsx";
-
-export default function Console() {
-   return (
-       <section className="ml-60 p-30 flex flex-1 h-screen ">
-           <Terminal />
-       </section>
-   )
-}
